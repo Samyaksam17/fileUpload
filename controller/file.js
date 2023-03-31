9d0b094b4c644a6b504beeee0cadc6b577bfe4df
Samyaksam17 <jsamyak407@gmail.com>
2023-03-31 16:37:17 +0530

    :rocket:UPDATED

--- controller/file.js
+++ controller/file.js
@@ -29,17 +29,17 @@ module.exports.fileStream = (req, res) => {
   const filePath = `uploadedVideos/${filename}`;
   const stat = fs.statSync(filePath);
   const fileSize = stat.size;
+
   const range = req.headers.range;
   if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-    const chunkSize = end - start + 1;
-    const file = fs.createReadStream(filePath, { start, end });
+    const chunkSize = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + chunkSize, fileSize - 1);
+    const contentLength = end - start + 1;
     const headers = {
       "Content-Range": `bytes ${start}-${end}/${fileSize}`,
       "Accept-Ranges": "bytes",
-      "Content-Length": chunkSize,
+      "Content-Length": contentLength,
       "Content-Type": "video/mp4",
     };
     res.writeHead(206, headers);
--- routes/file.js
+++ routes/file.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs")
+const fs = require("fs");
 
 // import controller
 const File = require("../controller/file");
@@ -8,7 +8,5 @@ const File = require("../controller/file");
 router.post("/upload", File.upload, File.fileUpload);
 router.get("/stream/:filename", File.fileStream);
 router.get("/download/:filename", File.fileDownload);
-
-
 
 module.exports = router;
